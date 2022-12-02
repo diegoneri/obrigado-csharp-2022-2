3e76006bd8b2def2675428a14d91b9c39c074d75
Curso de Informatica <informatica@adolphoberezin.com>
2022-12-02 19:34:01 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double numero = 0;
+bool ehPar = false;
+
+Console.WriteLine("=============================");
+Console.WriteLine("========Par ou ímpar=========");
+Console.WriteLine("=============================");
+
+Console.Write("Informe um número:\t");
+
+numero = Convert.ToDouble(Console.ReadLine()!);
+ehPar = (numero % 2 == 0);
+
+if (ehPar)
+{
+    Console.WriteLine($"O número {numero} é par");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é ímpar");
+}
